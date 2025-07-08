47d7a793726bd44cd3ceb23b94a91e49ef8610b0
Santiago Gonzalez <saanti95.g@hotmail.com>
2025-07-08 19:18:29 -0300

    Primer commit : subir proyecto Django

--- ejemplo_django/urls.py
+++ ejemplo_django/urls.py
@@ -17,9 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mi-primer-app/', include ('mi_primer_app.urls')),
+    path('', include('mi_primer_app.urls')),
     # path('hola-mundo/', saludo), 
 ]
--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import saludo
+from .views import saludo, saludo_con_template, crear_familiar, inicio
 
 urlpatterns = [
+    path('', inicio, name= 'inicio'),
     path('hola-mundo/', saludo),
+    path('hola-mundo-template/', saludo_con_template),
+    path('crear-familiar/<str:nombre>/', crear_familiar),
 ]
