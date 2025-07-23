a809d114276dc145b233e4140bd9a8f30b322908
Santiago Gonzalez <saanti95.g@hotmail.com>
2025-07-23 17:33:10 -0300

    despues de recuperar archivo

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 
+from .forms import RegistreForm
+
 class UserRegisterView(CreateView):
     model = User
-    form_class = UserCreationForm
+    form_class = RegistreForm
     template_name = 'usuarios/register.html'
     success_url = reverse_lazy('login')
