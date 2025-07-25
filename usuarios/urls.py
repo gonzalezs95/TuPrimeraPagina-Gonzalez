55bd0f596854f90975499cc306126c527bdabf70
Santiago Gonzalez <saanti95.g@hotmail.com>
2025-07-25 19:52:36 -0300

    terminacion del proyecto

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import CustomLogoutView, UserRegisterView, CustomLoginView, ProfileView, AvatarUpdateView
+from .views import UserRegisterView, CustomLoginView, CustomLogoutView, ProfileView, AvatarUpdateView, ProfileUpdateView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('update-avatar/', AvatarUpdateView.as_view(), name='avatar_update'),
+    path("profile/edit/", ProfileUpdateView.as_view(), name="edit_profile"),
+    path('update-avatar/', AvatarUpdateView.as_view(), name='avatar-update'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView, UpdateView, DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import authenticate, login,  update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
-from .forms import RegisterForm, AvatarForm
+from .forms import RegisterForm, AvatarForm, ProfileUpdateForm
 from django.views import View
 from .models import Avatar
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class UserRegisterView(CreateView):
     model = User
@@ -22,67 +22,31 @@ class UserRegisterView(CreateView):
     def form_valid(self, form):
         response = super().form_valid(form)
         user = authenticate(
-            username=form.cleaned_data['username'],
-            password=form.cleaned_data['password1'])
+            username=form.cleaned_data["username"], password=form.cleaned_data["password1"])
         if user:
             login(self.request, user)
         return response
 
+
 class CustomLoginView(LoginView):
     template_name = 'usuarios/login.html'
-    
-    def get_success_url(self):
-        return reverse_lazy('inicio')  # Redirect to home after login
-    
-class CustomLogoutView(LogoutView):
-    next_page = reverse_lazy('login')  # Redirect to login after logout
 
-class ProfileView(DetailView):
+    def get_success_url(self):
+        return reverse_lazy('inicio')
+
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
+
+
+class ProfileView(LoginRequiredMixin, DetailView):
     model = User
     template_name = 'usuarios/profile.html'
     context_object_name = 'user'
 
     def get_object(self):
-        return self.request.user  # Return the current logged-in user
-    
-class ProfileUpdateView(View):
-    template_name = "user/edit_profile.html"
-    success_url = reverse_lazy('profile')
+        return self.request.user
 
-    def get(self, request, *args, **kwargs):
-        user_form = ProfileUpdateView(instance=request.user)
-        avatar, _ = Avatar.objects.get_or_create(user=request.user)
-        avatar_form = AvatarForm(instance=avatar)
-        password_form = PasswordChangeForm(request.user)
-        context = {
-            'user_form': user_form,
-            'avatar_form': avatar_form,
-            'password_form': password_form
-        }
-        return render(request, self.template_name, context)
-    
-    def post(self, request, *args, **kwargs):
-        user_form = ProfileUpdateView(request.POST, instance=request.user)
-        avatar, _ = Avatar.objects.get_or_create(user=request.user)
-        avatar_form = AvatarForm(request.POST, request.FILES, instance=avatar)
-        password_form = PasswordChangeForm(request.user, request.POST)
-
-        if "update_profile" in request.POST:
-            if user_form.is_valid() and avatar_form.is_valid():
-                user_form.save()
-                avatar_form.save()
-                return redirect(self.success_url)
-        elif "change_password" in request.POST:
-            if password_form.is_valid():
-                user = password_form.save()
-                update_session_auth_hash(request, user)
-                return redirect(self.success_url)
-        context = {
-            'user_form': user_form,
-            'avatar_form': avatar_form,
-            'password_form': password_form
-        }
-        return render(request, self.template_name, context)
 
 class AvatarUpdateView(UpdateView):
     model = Avatar
@@ -91,11 +55,52 @@ class AvatarUpdateView(UpdateView):
     success_url = reverse_lazy('profile')
 
     def get_object(self, queryset=None):
+        # Obtiene o crea el avatar asociado al usuario actual
         avatar, created = Avatar.objects.get_or_create(user=self.request.user)
         return avatar
 
 
+class ProfileUpdateView(LoginRequiredMixin, View):
+    template_name = "usuarios/edit_profile.html"
+    success_url = reverse_lazy("profile")
 
-class ProfileUpdateView(UpdateView):
-    model = User
-    
+    def get(self, request, *args, **kwargs):
+        user_form = ProfileUpdateForm(instance=request.user)
+        # Obtenemos o creamos el avatar del usuario
+        avatar, _ = Avatar.objects.get_or_create(user=request.user)
+        avatar_form = AvatarForm(instance=avatar)
+        password_form = PasswordChangeForm(request.user)
+        context = {
+            "user_form": user_form,
+            "avatar_form": avatar_form,
+            "password_form": password_form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        # Inicializamos formularios con la data enviada.
+        user_form = ProfileUpdateForm(request.POST, instance=request.user)
+        avatar, _ = Avatar.objects.get_or_create(user=request.user)
+        avatar_form = AvatarForm(request.POST, request.FILES, instance=avatar)
+        password_form = PasswordChangeForm(request.user, request.POST)
+
+        if "update_profile" in request.POST:
+            # Se envió el formulario para actualizar datos y avatar.
+            if user_form.is_valid() and avatar_form.is_valid():
+                user_form.save()
+                avatar_form.save()
+                return redirect(self.success_url)
+        elif "change_password" in request.POST:
+            # Se envió el formulario para cambiar contraseña.
+            if password_form.is_valid():
+                user = password_form.save()
+                # Importante: actualizar la sesión para que el usuario no se desconecte.
+                update_session_auth_hash(request, user)
+                return redirect(self.success_url)
+
+        context = {
+            "user_form": user_form,
+            "avatar_form": avatar_form,
+            "password_form": password_form,
+        }
+        return render(request, self.template_name, context)
